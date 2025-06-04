4864df3927b6fba79c5c5e5eeb4c514de03b0911
GiteshC <42336770+GiteshC@users.noreply.github.com>
2025-06-04 15:21:12 +0530

    homepage function and banner component done

--- kalpataru-signature/src/components/BannerComponent.tsx
+++ kalpataru-signature/src/components/BannerComponent.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 
-const BannerComponent = ({ isPageLoad, mainHeading, para }: { isPageLoad: boolean, mainHeading: String, para:  String },  ) => {
+const BannerComponent = ({ isPageLoad, bannerVideo, mainHeading, para }: { isPageLoad: boolean, bannerVideo: String, mainHeading: String, para:  String },  ) => {
   return (
     <section className="homebannerSec projectdetBanner">
       <div className="videoBox">
         <video autoPlay muted loop playsInline preload="metadata">
-          <source src="/images/homepage-video.mp4" type="video/mp4" />
+          <source src={bannerVideo} type="video/mp4" />
         </video>
       </div>
       <div className="homebannerWrapper homebannerContent">
         <div className="secHeading philosopy-banner">
-          <h1
-            className={`swift-up-text ${isPageLoad ? "new-swift-up-text" : ""}`}
-          >
+          <h1 className={`swift-up-text ${isPageLoad ? "new-swift-up-text" : ""}`} >
             {mainHeading}
           </h1>
           <p className="bannerTextanimation">
