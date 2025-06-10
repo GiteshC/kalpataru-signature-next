a2d4c7a8fe366d1a65570d7674dd5d6fae47c522
Saniya8-01 <saniya.zad@theminimalist.in>
2025-06-10 14:11:35 +0530

    updated project details and added bannerComponent

---
+++ kalpataru-signature/src/components/BannerComponent.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import usePageLoad from "@/hooks/usePageLoad";
+
+const BannerComponent = ({
+  videoUrl,
+  mainHeading,
+  para,
+}: {
+  videoUrl: string,
+  mainHeading: string;
+  para: string;
+}) => {
+  const { isPageLoad } = usePageLoad();
+  const spanData = mainHeading.split(" ");
+  console.log(spanData);
+  console.log(videoUrl);
+  
+
+  return (
+    <section className="homebannerSec projectdetBanner">
+      <div className="videoBox">
+        <video autoPlay muted loop playsInline preload="metadata">
+          <source src={videoUrl} type="video/mp4" />
+        </video>
+      </div>
+      <div className="homebannerWrapper homebannerContent">
+        <div className="secHeading philosopy-banner">
+          <h1
+            className={`swift-up-text ${isPageLoad ? "new-swift-up-text" : ""}`}
+          >
+            {spanData.map((el, i) => (
+              <span key={el} >
+                <i style={{ transitionDelay: `${i * 0.4}s`}}>{el}&nbsp;</i>
+              </span>
+            ))}
+          </h1>
+          <p
+            className={`bannerTextanimation ${
+              isPageLoad ? "bannerTextanimationnew" : ""
+            }`}
+          >
+            <span>{para}</span>
+          </p>
+        </div>
+      </div>
+      <div className="scrollText">
+        <h3>Scroll to Explore</h3>
+      </div>
+    </section>
+  );
+};
+
+export default BannerComponent;
