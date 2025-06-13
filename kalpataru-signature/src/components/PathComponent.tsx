2c91d0d1d8a6927ec3a1651421fe19cf524674ff
sachin gour <sachinMinimalist001>
2025-06-13 10:51:34 +0530

    modify the path component

--- kalpataru-signature/src/components/PathComponent.tsx
+++ kalpataru-signature/src/components/PathComponent.tsx
@@ -1,11 +1,23 @@
 import Link from "next/link";
 import React from "react";
 
-const PathComponent = ({ pageName }: { pageName: string }) => {
+const PathComponent = ({
+  pageName,
+  flag,
+  subpage,
+}: {
+  pageName: string;
+  flag: boolean;
+  subpage: string;
+}) => {
   return (
     <div className="breadcrumbsSec">
       <p>
-        <Link href={"/"}>Home &nbsp; / &nbsp;</Link> {pageName}
+        <Link href={"/"}>Home &nbsp; / &nbsp;</Link>
+        {flag && (
+          <Link href="signature-residences.html">{subpage} &nbsp; /&nbsp;</Link>
+        )}
+        {pageName}
       </p>
     </div>
   );
