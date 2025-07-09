6d2b11f8b73626d37e2273e3b53d295cc2b76339
Saniya8-01 <saniya.zad@theminimalist.in>
2025-07-09 21:44:38 +0530

    Update BannerComponent.tsx

--- kalpataru-signature/src/components/BannerComponent.tsx
+++ kalpataru-signature/src/components/BannerComponent.tsx
@@ -34,73 +34,38 @@ const BannerComponent = ({ bannerData }: BannerProps) => {
       <div className="videoBox">
         {videoUrl || bannerData?.banner_options?.[0]?.banner_video_field ? (
           <video autoPlay muted loop playsInline preload="metadata">
-            <source
-              src={`${
-                videoUrl
-                  ? videoUrl
-                  : bannerData?.banner_options?.[0]?.banner_video_field
-              }`}
-              type="video/mp4"
-            />
+            <source src={`${videoUrl ? videoUrl : bannerData?.banner_options?.[0]?.banner_video_field}`} type="video/mp4" />
           </video>
         ) : (
-          <Image
-            src={`${
-              imageUrl
-                ? imageUrl
-                : bannerData?.banner_options?.[0]?.banner_image_field?.url
-            }`}
-            alt=""
-            width={0}
-            height={0}
-            style={{ width: "100%", height: "auto" }}
-          />
+          <Image src={`${imageUrl ? imageUrl : bannerData?.banner_options?.[0]?.banner_image_field?.url}`} alt="" width={bannerData?.banner_options?.[0]?.banner_image_field?.width} height={bannerData?.banner_options?.[0]?.banner_image_field?.height} style={{ width: "100%", height: "auto" }} />
         )}
       </div>
       <div className="homebannerWrapper homebannerContent">
         <div className="secHeading philosopy-banner">
-          <h1
-            className={`swift-up-text ${isPageLoad ? "new-swift-up-text" : ""}`}
-          >
+          <h1 className={`swift-up-text ${isPageLoad ? "new-swift-up-text" : ""}`}>
             {spanData?.map((el: string, i: number) => (
               <span key={el}>
                 <i style={{ transitionDelay: `${i * 0.4}s` }}>{el}&nbsp;</i>
               </span>
             ))}
           </h1>
-          <p
-            className={`bannerTextanimation ${
-              isPageLoad ? "bannerTextanimationnew" : ""
-            }`}
-          >
+          <p className={`bannerTextanimation ${isPageLoad ? "bannerTextanimationnew" : ""}`}>
             <span>{para}</span>
           </p>
           {bannerSubHeading && (
-            <h3
-              className={`bannerTextanimation ${
-                isPageLoad ? "bannerTextanimationnew" : ""
-              }`}
-            >
+            <h3 className={`bannerTextanimation ${isPageLoad ? "bannerTextanimationnew" : ""}`}>
               <span>{bannerSubHeading}</span>
             </h3>
           )}
           {address && (
-            <h3
-              className={`bannerTextanimation ${
-                isPageLoad ? "bannerTextanimationnew" : ""
-              }`}
-            >
+            <h3 className={`bannerTextanimation ${isPageLoad ? "bannerTextanimationnew" : ""}`}>
               <span>{address}</span>
             </h3>
           )}
         </div>
       </div>
       <div className={`scrollText ${isPageLoad ? "scrollTextnew" : ""}`}>
-        <h3>
-          {mainHeading.trim().toLocaleUpperCase() === "SIGNATURE RESIDENCES"
-            ? "Explore projects"
-            : "Scroll to Explore"}
-        </h3>
+        <h3>{mainHeading.trim().toLocaleUpperCase() === "SIGNATURE RESIDENCES" ? "Explore projects" : "Scroll to Explore"}</h3>
       </div>
     </section>
   );
